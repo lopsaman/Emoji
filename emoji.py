c58ef20d2c802afc9f1750985036e80d54eb1888
lopsaman <72528639+lopsaman@users.noreply.github.com>
2021-05-05 17:05:52 -0400

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,10 @@
+import emotlib
+print(emotlib.emoji())
+
+print(emotlib.emoji(category='food-fruit'))
+
+print(emotlib.emoji(num=3))
+
+print(emotlib.emoji(num=3, sep= ' ~ '))
+
+print(emotlib.emojify('I\'m :man_technologist:'))
